f0e21b20b1e7e6c994090c656ce15086e09de858
slabmarketeu <slabmarketeu@gmail.com>
2025-11-19 21:01:51 +0100

    Set default theme to dark for new users
    - Add dark class to Html element in _document.tsx to prevent light theme flash on initial load
    - Ensures new users see dark theme immediately without any light theme flicker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" suppressHydrationWarning>
+    <Html lang="en" className="dark" suppressHydrationWarning>
       <Head>
         {/*
           CRITICAL: DO NOT REMOVE THIS SCRIPT
